cfd84e1d90048671405e7ae7253cdea68f29a80f
BuildTools <unconfigured@null.spigotmc.org>
2023-01-02 16:00:15 +0900

    improvement(and didnt work)

--- eggtoast_factorit/api/loader/guiloader.lua
+++ eggtoast_factorit/api/loader/guiloader.lua
@@ -3,13 +3,14 @@ guiload = {
     loadgui = function ()
         guiload.list2 = love.filesystem.getDirectoryItems("/guimap/")
         for i=1,#guiload.list2 do
-            loadphase2="/guimap/"..guiload.list2[i]
             tmp=require("/guimap/"..string.gsub(guiload.list2[i],".lua",""))
             table.insert(guiload.list,tmp)
         end
     end,
     hotswap=function ()
         print("hotswapping gui")
+        guiload.list={}
+        gui.tempguinow=nil
         guiload.loadgui()
         print("hotswapped gui")
     end,
