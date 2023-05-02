5d60c42a09bcaef4357cb0c1df8fd44ea8ac8bb9
BuildTools <unconfigured@null.spigotmc.org>
2023-05-02 21:28:23 +0900

    added object placing lager sight thing, click 2 place, and more

--- eggtoast_factorit/api/player.lua
+++ eggtoast_factorit/api/player.lua
@@ -9,33 +9,39 @@ plr={
     hotselect=1,
     itemsys = function ()
         grabbing=plr.inventory[plr.hotselect+32]
+        local mx,my = mouse.getPos()
         if #plr.inventory < 42 then
                 table.insert(plr.inventory,{id=nil})
         end
-        if controller.abutton() then
-            if table.contains(obj.placeable,grabbing.id) then
-                obj.make(grabbing.id.."plcd",plr.x,plr.y,obj.placeablenbt[indexOf(obj.placeable,grabbing.id)],grabbing.id)
+        if table.contains(obj.placeable,grabbing.id) then
+            local sz = ((table.contains(obj.sizemapid,grabbing.id.."plcd")) and (obj.sizemap[indexOf(obj.sizemapid,grabbing.id.."plcd")]) or (0.4))
+            if mouse.ison(1) then
+                local tmpx,tmpy = cam:worldCoords(mx-(250*sz),my-(250*sz))
+                obj.make(grabbing.id.."plcd",tmpx,tmpy,obj.placeablenbt[indexOf(obj.placeable,grabbing.id)],grabbing.id)
                 plr.inventory[plr.hotselect+32]={id=nil}
             end
         end
     end,
     itemdraw = function ()
+        local dx,dy = love.graphics.getDimensions()
+        dx = dx /2
+        dy = dy /2
         if plr.rot=="left"then
-            love.graphics.draw(gr.texture.gettex("character1"),plr.x-25,plr.y-50,0,0.1,0.1)
+            love.graphics.draw(gr.texture.gettex("character1"),dx-25,dy-50,0,0.1,0.1)
         else
-            love.graphics.draw(gr.texture.gettex("character1"),plr.x+25,plr.y-50,0,-0.1,0.1)
+            love.graphics.draw(gr.texture.gettex("character1"),dx+25,dy-50,0,-0.1,0.1)
         end
         if plr.handrot==1 then
             if plr.rot=="left"then
-                    love.graphics.draw(gr.texture.gettex("hand"),plr.x+4-25,plr.y+25,11,0.1,0.1)
+                    love.graphics.draw(gr.texture.gettex("hand"),dx+4-25,dy+25,11,0.1,0.1)
                 else
-                    love.graphics.draw(gr.texture.gettex("hand"),plr.x+25,plr.y+25,-11,-0.1,0.1)
+                    love.graphics.draw(gr.texture.gettex("hand"),dx+25,dy+25,-11,-0.1,0.1)
                 end
             else
                 if plr.rot=="left"then
-                    love.graphics.draw(gr.texture.gettex("hand"),plr.x+10-25,plr.y+22-50,0,0.1,0.1)
+                    love.graphics.draw(gr.texture.gettex("hand"),dx+10-25,dy+22-50,0,0.1,0.1)
                 else
-                    love.graphics.draw(gr.texture.gettex("hand"),plr.x+13,plr.y+22-50,0,-0.1,0.1)
+                    love.graphics.draw(gr.texture.gettex("hand"),dx+13,dy+22-50,0,-0.1,0.1)
                 end
             end
             if not (plr.inventory[plr.hotselect+32].id ==nil) then
@@ -46,10 +52,17 @@ plr={
                 local yoff2=(plr.rot=="left") and 0 or 25
                 local diroff=(plr.rot=="left") and 0 or 10
             if plr.handrot==1 then
-                love.graphics.draw(gr.texture.gettex(plr.inventory[plr.hotselect+32].id),plr.x+12+xoff+xoff2,plr.y-50+yoff2,45+diroff,sx,sy)
+                love.graphics.draw(gr.texture.gettex(plr.inventory[plr.hotselect+32].id),dx+12+xoff+xoff2,dy-50+yoff2,45+diroff,sx,sy)
             else
-                love.graphics.draw(gr.texture.gettex(plr.inventory[plr.hotselect+32].id),plr.x-32+xoff,plr.y+20-50,0,sx,0.1)
+                love.graphics.draw(gr.texture.gettex(plr.inventory[plr.hotselect+32].id),dx-32+xoff,dy+20-50,0,sx,0.1)
             end
+        end
+        if table.contains(obj.placeable,grabbing.id) then
+            local mx,my = mouse.getPos()
+            love.graphics.setColor(.25,1,.25)
+            local sz = ((table.contains(obj.sizemapid,grabbing.id.."plcd")) and (obj.sizemap[indexOf(obj.sizemapid,grabbing.id.."plcd")]) or (0.4))
+            love.graphics.draw(gr.texture.gettex(grabbing.id.."plcd"),mx-(250*sz),my-(250*sz),0,sz,sz)
+            love.graphics.setColor(1,1,1)
         end
     end
 }
