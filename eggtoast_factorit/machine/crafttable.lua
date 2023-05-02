ecd7413716fe8df24ddbf6537e764155ff030aa6
BuildTools <unconfigured@null.spigotmc.org>
2023-05-02 21:48:48 +0900

    random

--- eggtoast_factorit/machine/crafttable.lua
+++ eggtoast_factorit/machine/crafttable.lua
@@ -134,25 +134,25 @@ crafttable={
     end,
     draw = function (i)
         if inv.open == 1 then
-        love.graphics.setColor(1,1,1)
-        love.graphics.print(lang.gettxt("item.crafttable.name"),55,245,0,0.5,0.5)
-        insqp=craftbenchsquarepos
-        mx,my = mouse.getPos()
-        for i = 1, #craftbenchsquarepos do
-            if ((mx - insqp[i].x) > 0) and ((mx - insqp[i].x) <= 25) and ((my - insqp[i].y) > 0) and ((my - insqp[i].y) <= 25) then        
-                craftbenchsquareposhover[i].hover=0
-            else
-                craftbenchsquareposhover[i].hover=1
+            love.graphics.setColor(1,1,1)
+            love.graphics.print(lang.gettxt("item.crafttable.name"),55,245,0,0.5,0.5)
+            insqp=craftbenchsquarepos
+            mx,my = mouse.getPos()
+            for i = 1, #craftbenchsquarepos do
+                if ((mx - insqp[i].x) > 0) and ((mx - insqp[i].x) <= 25) and ((my - insqp[i].y) > 0) and ((my - insqp[i].y) <= 25) then        
+                    craftbenchsquareposhover[i].hover=0
+                else
+                    craftbenchsquareposhover[i].hover=1
+                end
+                gr.draw.square(craftbenchsquarepos[i].x,craftbenchsquarepos[i].y,30,30,"fill",craftbenchsquareposhover[i].hover,craftbenchsquareposhover[i].hover,craftbenchsquareposhover[i].hover)
+                ai=i
+                for i = 1,#craftbenchx do 
+                    gr.draw.square(craftbenchx[i],craftbenchsquarepos[ai].y,30,30,"fill",1,1,1)
+                end
             end
-            gr.draw.square(craftbenchsquarepos[i].x,craftbenchsquarepos[i].y,30,30,"fill",craftbenchsquareposhover[i].hover,craftbenchsquareposhover[i].hover,craftbenchsquareposhover[i].hover)
-            ai=i
-            for i = 1,#craftbenchx do 
-                gr.draw.square(craftbenchx[i],craftbenchsquarepos[ai].y,30,30,"fill",1,1,1)
-            end
+            crtdrawrec()
+            love.graphics.setColor(1,1,1)
         end
-        crtdrawrec()
-        love.graphics.setColor(1,1,1)
-    end
     end,
     toggleopen = function(i)
         if obj.list[i].nbt.craftopen==true then
@@ -160,14 +160,14 @@ crafttable={
             plr.craftopen=false
             crafteruse=false
             plr.craftopeni=nil
-            print("craftoff")
+            print("craftoff "..i)
         else
             if plr.craftopen==false then
             obj.list[i].nbt.craftopen=true
             plr.craftopen=true
             crafteruse=false
             plr.craftopeni=i
-            print("crafton")
+            print("crafton "..i)
             end
         end
     end
