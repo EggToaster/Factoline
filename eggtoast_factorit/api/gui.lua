9ae90e1775c42cde620bd81d9e00bfab4ca47f36
EggToaster <53361644+TV-Toshikawa@users.noreply.github.com>
2023-11-22 17:43:25 +0900

    fixed function call(took me month)

--- eggtoast_factorit/api/gui.lua
+++ eggtoast_factorit/api/gui.lua
@@ -5,6 +5,127 @@ gui = {
     end,
     draw = function (tbl)
         gui.tempguinow = tbl
+        if table.haskey(tbl,"button") then
+            for i = 1,#tbl.button do
+                local this = tbl.button[i]
+                local ofs={0,0,0,0}
+                local stenreset = false
+                if table.eachContain(tbl.stencil.stenciltag,this.tag) then
+                    stenreset = true
+                    gui.script.stencilTag=table.indexOfEach(tbl.stencil.stenciltag,this.tag)
+                    love.graphics.stencil(tbl.stencil.try,"replace",1)
+                    love.graphics.setStencilTest("greater", 0)
+                end
+                if this.slide then
+                    ofs[2] = -1*gui.script.sliderpos
+                end
+                local tmp = {this.pos[1],this.pos[2],this.size[1],this.size[2]}
+                for ii = 1,4 do
+                    if type(tmp[ii]) == "number" then
+                        tmp[ii] =  {"dummy",tmp[ii]}
+                    end
+                end 
+                gui.script.setctbl(this.bg)
+                love.graphics.rectangle("fill", gui.script.func[tmp[1][1]](tmp[1][2]) + ofs[1], gui.script.func[tmp[2][1]](tmp[2][2]) + ofs[2], gui.script.func[tmp[3][1]](tmp[3][2]) +ofs[3], gui.script.func[tmp[4][1]](tmp[4][2]) +ofs[4])
+                gui.script.setctbl(this.color)
+                love.graphics.rectangle("line",gui.script.func[tmp[1][1]](tmp[1][2])+ofs[1],gui.script.func[tmp[2][1]](tmp[2][2])+ofs[2],gui.script.func[tmp[3][1]](tmp[3][2])+ofs[3],gui.script.func[tmp[4][1]](tmp[4][2])+ofs[4])
+                gui.script.setctbl(this.txc)
+                local txt = this.text
+                if type(txt)=="function" then
+                    txt = txt()
+                end
+                    if string.sub(txt,1,1)=="#" then
+                        txt = lang.gettxt(string.sub(txt,2,string.len(txt)))
+                        love.graphics.print(txt,this.pos[1]+ofs[1],this.pos[2]+ofs[2]-5,0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
+                    else
+                    if string.sub(txt,1,1)=="?" then
+                        love.graphics.draw(texture.gettex(string.sub(txt,2,string.len(txt))),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
+                    else
+                        love.graphics.print(txt,this.pos[1]+ofs[1],this.pos[2]+ofs[2]-10,0,this.size[1]+ofs[3],this.txs[2]+ofs[4])
+                    end
+                end
+                if stenreset then
+                    love.graphics.setStencilTest()
+                end
+            end
+        end
+        if table.haskey(tbl,"rect") then
+            for i = 1,#tbl.rect do
+                local object = tbl.rect[i]
+                local ofs={0,0,0,0}
+                local stenreset = false
+                if table.eachContain(tbl.stencil.stenciltag,object.tag) then
+                    stenreset = true
+                    gui.script.stencilTag=table.indexOfEach(tbl.stencil.stenciltag,object.tag)
+                    love.graphics.stencil(tbl.stencil.try,"replace",1)
+                    love.graphics.setStencilTest("greater", 0)
+                end
+                if object.slide then
+                    ofs[2] = -1*gui.script.sliderpos
+                end
+                local tmp = {object.pos[1],object.pos[2],object.size[1],object.size[2]}
+                for ii = 1,4 do
+                    if type(tmp[ii]) == "number" then
+                        tmp[ii] =  {"dummy",tmp[ii]}
+                    end
+                end 
+                gui.script.setctbl(object.bg)
+                love.graphics.rectangle("fill", gui.script.func[tmp[1][1]](tmp[1][2]) + ofs[1], gui.script.func[tmp[2][1]](tmp[2][2]) + ofs[2], gui.script.func[tmp[3][1]](tmp[3][2]) +ofs[3], gui.script.func[tmp[4][1]](tmp[4][2]) +ofs[4])
+                gui.script.setctbl(object.color)
+                love.graphics.rectangle("line",gui.script.func[tmp[1][1]](tmp[1][2])+ofs[1],gui.script.func[tmp[2][1]](tmp[2][2])+ofs[2],gui.script.func[tmp[3][1]](tmp[3][2])+ofs[3],gui.script.func[tmp[4][1]](tmp[4][2])+ofs[4])
+                if stenreset then
+                    love.graphics.setStencilTest()
+                end
+            end
+        end
+        if table.haskey(tbl,"recttxt") then
+            for i = 1,#tbl.recttxt do
+                local this = tbl.recttxt[i]
+                local object=this
+                local ofs={0,0,0,0}
+                local stenreset = false
+                if table.eachContain(tbl.stencil.stenciltag,this.tag) then
+                    stenreset = true
+                    gui.script.stencilTag=table.indexOfEach(tbl.stencil.stenciltag,this.tag)
+                    love.graphics.stencil(tbl.stencil.try,"replace",1)
+                    love.graphics.setStencilTest("greater", 0)
+                end
+                if this.slide then
+                    ofs[2] = -1*gui.script.sliderpos
+                end
+                local tmp = {object.pos[1],object.pos[2],object.size[1],object.size[2]}
+                for ii = 1,4 do
+                    if type(tmp[ii]) == "number" then
+                        tmp[ii] =  {"dummy",tmp[ii]}
+                    end
+                end 
+                gui.script.setctbl(object.bg)
+                love.graphics.rectangle("fill", gui.script.func[tmp[1][1]](tmp[1][2]) + ofs[1], gui.script.func[tmp[2][1]](tmp[2][2]) + ofs[2], gui.script.func[tmp[3][1]](tmp[3][2]) +ofs[3], gui.script.func[tmp[4][1]](tmp[4][2]) +ofs[4])
+                gui.script.setctbl(object.color)
+                love.graphics.rectangle("line",gui.script.func[tmp[1][1]](tmp[1][2])+ofs[1],gui.script.func[tmp[2][1]](tmp[2][2])+ofs[2],gui.script.func[tmp[3][1]](tmp[3][2])+ofs[3],gui.script.func[tmp[4][1]](tmp[4][2])+ofs[4])
+                local txt = this.text
+                if type(txt)=="function" then
+                    if string.sub(txt(),1,1) == "?" then
+                        print(string.sub(txt(),2,string.len(txt())))
+                        love.graphics.draw(texture.gettex(string.sub(txt(),2,string.len(txt()))),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
+                    else
+                        love.graphics.print(txt(),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
+                    end
+                else
+                    if string.sub(txt,1,1)=="#" then
+                        txt = lang.gettxt(string.sub(txt,2,string.len(txt)))
+                    end
+                    if string.sub(txt,1,1)=="?" then
+                        love.graphics.draw(texture.gettex(string.sub(txt,2,string.len(txt))),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
+                    else
+                        love.graphics.print(txt,this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
+                    end
+                end
+                if stenreset then
+                    love.graphics.setStencilTest()
+                end
+            end
+        end
         if table.haskey(tbl,"text") then
             for i = 1,#tbl.text do
                 local this = tbl.text[i]
@@ -31,7 +152,12 @@ gui = {
                 gui.script.setctbl(this.color)
                 local txt = this.text
                 if type(txt)=="function" then
-                    love.graphics.draw(txt(),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.size[1]+ofs[3],this.size[2]+ofs[4])
+                    if string.sub(txt(),1,1) == "?" then
+                        print(string.sub(txt(),2,string.len(txt())))
+                        love.graphics.draw(texture.gettex(string.sub(txt(),2,string.len(txt()))),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.size[1]+ofs[3],this.size[2]+ofs[4])
+                    else
+                        love.graphics.print(txt(),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.size[1]+ofs[3],this.size[2]+ofs[4])
+                    end
                 else
                     if string.sub(txt,1,1)=="#" then
                         txt = lang.gettxt(string.sub(txt,2,string.len(txt)))
@@ -42,122 +168,6 @@ gui = {
                         love.graphics.print(txt,this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.size[1]+ofs[3],this.size[2]+ofs[4])
                     end
                 end
-                if stenreset then
-                    love.graphics.setStencilTest()
-                end
-            end
-        end
-        if table.haskey(tbl,"button") then
-            for i = 1,#tbl.button do
-                local this = tbl.button[i]
-                local ofs={0,0,0,0}
-                local stenreset = false
-                if table.eachContain(tbl.stencil.stenciltag,this.tag) then
-                    stenreset = true
-                    gui.script.stencilTag=table.indexOfEach(tbl.stencil.stenciltag,this.tag)
-                    love.graphics.stencil(tbl.stencil.try,"replace",1)
-                    love.graphics.setStencilTest("greater", 0)
-                end
-                if this.slide then
-                    ofs[2] = -1*gui.script.sliderpos
-                end
-                local tmpthis = this
-                local tmp = {this.pos[1],this.pos[2],this.size[1],this.size[2]}
-                for ii = 1,4 do
-                    if type(tmp[ii])=="string" then
-                        this[(3>ii) and "pos" or "size"][(3>ii) and (ii) or (ii-2)] = gui.script.func[tmp[ii]].func()
-                    else
-                    if type(tmp[ii])=="function" then
-                        this[(3>ii) and "pos" or "size"][(3>ii) and (ii) or (ii-2)] = this[(3>ii) and "pos" or "size"][(3>ii) and (ii) or (ii-2)]()
-                    end
-                end
-                end
-                gui.script.setctbl(this.bg)
-                love.graphics.rectangle("fill",this.pos[1]+ofs[1],this.pos[2]+ofs[2],this.size[1]+ofs[3],this.size[2]+ofs[4])
-                gui.script.setctbl(this.color)
-                love.graphics.rectangle("line",this.pos[1]+ofs[1],this.pos[2]+ofs[2],this.size[1]+ofs[3],this.size[2]+ofs[4])
-                gui.script.setctbl(this.txc)
-                local txt = this.text
-                if type(txt)=="function" then
-                    txt = txt()
-                end
-                    if string.sub(txt,1,1)=="#" then
-                        txt = lang.gettxt(string.sub(txt,2,string.len(txt)))
-                        love.graphics.print(txt,this.pos[1]+ofs[1],this.pos[2]+ofs[2]-5,0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
-                    else
-                    if string.sub(txt,1,1)=="?" then
-                        love.graphics.draw(texture.gettex(string.sub(txt,2,string.len(txt))),this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
-                    else
-                        love.graphics.print(txt,this.pos[1]+ofs[1],this.pos[2]+ofs[2]-10,0,this.size[1]+ofs[3],this.txs[2]+ofs[4])
-                    end
-                end
-                local this = tmpthis
-                if stenreset then
-                    love.graphics.setStencilTest()
-                end
-            end
-        end
-        if table.haskey(tbl,"rect") then
-            for i = 1,#tbl.rect do
-                local object = tbl.rect[i]
-                local ofs={0,0,0,0}
-                local stenreset = false
-                if table.eachContain(tbl.stencil.stenciltag,object.tag) then
-                    stenreset = true
-                    gui.script.stencilTag=table.indexOfEach(tbl.stencil.stenciltag,object.tag)
-                    love.graphics.stencil(tbl.stencil.try,"replace",1)
-                    love.graphics.setStencilTest("greater", 0)
-                end
-                if object.slide then
-                    ofs[2] = -1*gui.script.sliderpos
-                end
-                local tmp = {object.pos[1],object.pos[2],object.size[1],object.size[2]}
-                for ii = 1,4 do
-                    if type(tmp[ii]) == "number" then
-                        tmp[ii] =  {"dummy",tmp[ii]}
-                    end
-                end
-                gui.script.setctbl(object.bg)
-                print(json.encode(tmp))
-                love.graphics.rectangle("fill",_G[tmp[1][1]](tmp[1][2])+ofs[1],_G[tmp[2][1]](tmp[2][2])+ofs[2],_G[tmp[3][1]](tmp[3][2])+ofs[3],_G[tmp[4][1]](tmp[4][2])+ofs[4])
-                gui.script.setctbl(object.color)
-                love.graphics.rectangle("line",_G[tmp[1][1]](tmp[1][2])+ofs[1],_G[tmp[2][1]](tmp[2][2])+ofs[2],_G[tmp[3][1]](tmp[3][2])+ofs[3],_G[tmp[4][1]](tmp[4][2])+ofs[4])
-                if stenreset then
-                    love.graphics.setStencilTest()
-                end
-            end
-        end
-        if table.haskey(tbl,"recttxt") then
-            for i = 1,#tbl.recttxt do
-                local this = tbl.recttxt[i]
-                local ofs={0,0,0,0}
-                local stenreset = false
-                if table.eachContain(tbl.stencil.stenciltag,this.tag) then
-                    stenreset = true
-                    gui.script.stencilTag=table.indexOfEach(tbl.stencil.stenciltag,this.tag)
-                    love.graphics.stencil(tbl.stencil.try,"replace",1)
-                    love.graphics.setStencilTest("greater", 0)
-                end
-                if this.slide then
-                    ofs[2] = -1*gui.script.sliderpos
-                end
-                local tmpthis = this
-                local tmp = {this.pos[1],this.pos[2],this.size[1],this.size[2]}
-                for ii = 1,4 do
-                    if type(tmp[ii])=="string" then
-                        this[(3>ii) and "pos" or "size"][(3>ii) and (ii) or (ii-2)] = gui.script.func[tmp[ii]].func()
-                    end
-                end
-                gui.script.setctbl(this.bg)
-                love.graphics.rectangle("fill",this.pos[1]+ofs[1],this.pos[2]+ofs[2],this.size[1]+ofs[3],this.size[2]+ofs[4])
-                gui.script.setctbl(this.color)
-                love.graphics.rectangle("line",this.pos[1]+ofs[1],this.pos[2]+ofs[2],this.size[1]+ofs[3],this.size[2]+ofs[4])
-                local txt = this.text
-                if string.sub(txt,1,1)=="#" then
-                    txt = lang.gettxt(string.sub(txt,2,string.len(txt)))
-                end
-                love.graphics.print(txt,this.pos[1]+ofs[1],this.pos[2]+ofs[2],0,this.txs[1]+ofs[3],this.txs[2]+ofs[4])
-                local this = tmpthis
                 if stenreset then
                     love.graphics.setStencilTest()
                 end
@@ -253,18 +263,15 @@ gui = {
         slideron = false,
         slideroffset=0,
         func = {
-            dx ={
-                func = function ()
-                    local dx,dy = love.graphics.getDimensions()
-                    return dx
-                end
-            },
-            dy ={
-                func = function ()
-                    local dx,dy = love.graphics.getDimensions()
-                    return dy
-                end
-            }
+            dx = function ()
+                local dx,dy = love.graphics.getDimensions()
+                return dx
+            end,
+            dy = function ()
+                local dx,dy = love.graphics.getDimensions()
+                return dy
+            end,
+            dummy = function(a) return a; end
         }
     },
     tempguinow = nil
