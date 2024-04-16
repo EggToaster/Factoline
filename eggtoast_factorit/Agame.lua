aa36cfecebae46ee9f5c446a73261b59e03586a7
EggToast <toshikawa907@gmail.com>
2024-04-16 19:33:52 +0900

    organized code, removed not needed line

--- eggtoast_factorit/Agame.lua
+++ eggtoast_factorit/Agame.lua
@@ -5,57 +5,55 @@ end
 
 function gamedraw()
     local dx,dy = love.graphics.getDimensions()
-    if not loading then
-        love.graphics.setBackgroundColor(1,1,1)
-        love.graphics.setColor(1,1,1)
-        cam:attach()
-        obj.draw()
-        cam:detach()
-        plr.itemdraw()
-        love.graphics.setColor(0,0,0,0.25)
-        love.graphics.rectangle("fill",0,0,900,100)
-        love.graphics.setColor(0,0,0,0.5)
-        love.graphics.rectangle("fill",100*plr.hotselect-100,0,100,100)
-        love.graphics.setColor(1,1,1)
-        for i = 1,9 do
-            if plr.inventory[i+32].id == nil then else
-                love.graphics.draw(texture.gettex(plr.inventory[i+32].id),(i-1)*100,0,0,0.2,0.2)
-            end
+    love.graphics.setBackgroundColor(1,1,1)
+    love.graphics.setColor(1,1,1)
+    cam:attach()
+    obj.draw()
+    cam:detach()
+    plr.itemdraw()
+    love.graphics.setColor(0,0,0,0.25)
+    love.graphics.rectangle("fill",0,0,900,100)
+    love.graphics.setColor(0,0,0,0.5)
+    love.graphics.rectangle("fill",100*plr.hotselect-100,0,100,100)
+    love.graphics.setColor(1,1,1)
+    for i = 1,9 do
+        if plr.inventory[i+32].id == nil then else
+            love.graphics.draw(texture.gettex(plr.inventory[i+32].id),(i-1)*100,0,0,0.2,0.2)
         end
-        love.graphics.setColor(0,0,0)
-        for i = 1,9 do
-            love.graphics.line(i*100,0,i*100,100)
-        end
-        love.graphics.rectangle("line",1,1,900,100)
-        love.graphics.setColor(1,1,1)
-        inv.draw()
-        if inv.open==1 then
-            love.graphics.draw(texture.gettex("crafttable"),295,170,0,0.065,0.065)
-        end
-        obj.drawobjgui()
-        love.graphics.setColor(.25,.25,.25)
-        love.graphics.rectangle("fill",dx-(150/512)*dx,110,(150/512)*dx,(150/512)*dx)
-        love.graphics.stencil(function ()
-            love.graphics.rectangle("fill",dx-(145/512)*dx,115,(145/512)*dx,(145/512)*dx)
-        end,"replace",1)
-        love.graphics.setStencilTest("greater", 0)
-        love.graphics.setColor(1,1,1)
+    end
+    love.graphics.setColor(0,0,0)
+    for i = 1,9 do
+        love.graphics.line(i*100,0,i*100,100)
+    end
+    love.graphics.rectangle("line",1,1,900,100)
+    love.graphics.setColor(1,1,1)
+    inv.draw()
+    if inv.open==1 then
+        love.graphics.draw(texture.gettex("crafttable"),295,170,0,0.065,0.065)
+    end
+    obj.drawobjgui()
+    love.graphics.setColor(.25,.25,.25)
+    love.graphics.rectangle("fill",dx-(150/512)*dx,110,(150/512)*dx,(150/512)*dx)
+    love.graphics.setColor(1,1,1)
+
+    love.graphics.stencil(function ()
+           love.graphics.rectangle("fill",dx-(145/512)*dx,115,(145/512)*dx,(145/512)*dx)
+    end,"replace",1)
+    love.graphics.setStencilTest("greater", 0)
         mnm:attach()
-        mnm:zoomTo(0.25)
-        mnm:lookAt(plr.x-(425/1215)*dx*4.17,plr.y-(-275/1715)*dy*3.8)
-        for i = 1,#obj.list do
-            local cx,cy = cam:cameraCoords(obj.list[i].x,obj.list[i].y)
-            if cx>=-1000 and cx<=dx+1000 and cy >= -1000 and cy <= dy+1000 then
-                obj.customdraw(obj.list[i])
+            mnm:zoomTo(0.25)
+            mnm:lookAt(plr.x-(425/1215)*dx*4.17,plr.y-(-275/1715)*dy*3.8)
+            for i = 1,#obj.list do
+                local cx,cy = cam:cameraCoords(obj.list[i].x,obj.list[i].y)
+                if cx>=-1000 and cx<=dx+1000 and cy >= -1000 and cy <= dy+1000 then
+                    obj.customdraw(obj.list[i])
+                end
             end
-        end
         mnm:detach()
         love.graphics.setColor(0,1,0,0.5)
         love.graphics.circle('fill',dx/1.27+(145/2000)*dx,dy/2+(-275/1700)*dy,10)
-        love.graphics.setStencilTest()
-    end
+    love.graphics.setStencilTest()
 end
-
 
 function gameupdate(dt)
     plr.itemsys()
