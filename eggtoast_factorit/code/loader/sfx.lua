38bf0b55ce7f5821382221e970dff94e93096bfb
EggToast <toshikawa907@gmail.com>
2024-04-16 17:23:36 +0900

    Fixed no sound bug

--- eggtoast_factorit/code/loader/sfx.lua
+++ eggtoast_factorit/code/loader/sfx.lua
@@ -1,7 +1,7 @@
 sfx = {
     list = {},
     loadsound = function ()
-        sfx.list2 = love.filesystem.getDirectoryItems("/sound/")
+        sfx.list2 = love.filesystem.getDirectoryItems("/res/sound/")
         for i=1,#sfx.list2 do
             tmp=love.audio.newSource("/res/sound/"..sfx.list2[i],"static")
             table.insert(sfx.list,tmp)
@@ -13,6 +13,6 @@ sfx = {
         end
     end,
     get=function (getthis)
-        return(sfx.list[indexOf(sfx.list2,getthis..".mp3")])        
+        return(sfx.list[indexOf(sfx.list2,getthis..".mp3")])
     end
 }
