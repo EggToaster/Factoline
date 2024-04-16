24b29eae8513152444e5c301d77f4e7263cf059f
EggToast <toshikawa907@gmail.com>
2024-04-16 18:46:29 +0900

    removed pointless part

--- eggtoast_factorit/code/spec/devicepreset.lua
+++ eggtoast_factorit/code/spec/devicepreset.lua
@@ -1,59 +1,13 @@
 local devicepreset = {
     pc={
         console = false,
-        dualscreen = false,
         padonly = false,
-        noclick = false,
-        device = "PC",
-        padbutton = {--xbox/nx/dualsense, no steering wheel here
-            "A","B","X","Y",
-            "L","R",
-            "ZL","ZR",
-            "SELECT","START",
-            "LSP","RSP"
-        },
-        specialpad = {
-            "DPAD",
-            "LS","RS"
-        },
-        detail = "personal computer"
+        noclick = false
     },
-    threeds={--threeds because table doesnt allow numbers
+    nx={ --possible, but cannot run on lovepotion atm
         console = true,
-        dualscreen = true,
         padonly = true,
-        noclick = true,
-        device = "3DS",
-        padbutton = {
-            "A","B","X","Y",
-            "L","R",
-            "ZL","ZR",--yes this game going to be only n3ds
-            "SELECT","START",
-        },
-        specialpad = {
-            "DPAD",
-            "LS","RS"
-        },
-        detail = "100% not nintendo licensed build tool"
-    },
-    switch={
-        console = true,
-        dualscreen = false,
-        padonly = true,
-        noclick = true,
-        device="NS",
-        padbutton = {
-            "A","B","X","Y",
-            "L","R",
-            "ZL","ZR",
-            "SELECT","START",
-            "LSP","RSP"
-        },
-        specialpad = {
-            "DPAD",
-            "LS","RS"
-        },
-        detail = "nx"
+        noclick = true
     }
 }
 return devicepreset
