bd29ac775e836fcec5856534ef74622bb4cb512b
EggToaster <53361644+TV-Toshikawa@users.noreply.github.com>
2024-05-30 16:08:27 +0900

    improved mouse.lua code

--- eggtoast_factorit/code/spec/mouse.lua
+++ eggtoast_factorit/code/spec/mouse.lua
@@ -1,50 +1,34 @@
-local meow
-local meow2 = false
-local meow3 = false
 mouse = {
+    l = false, r = true,_t=false,
     getPos = function ()
         return mouse.pos[1],mouse.pos[2]
     end,
     tick = function (dt)
-        local x = mouse.pos[1]
-        local y = mouse.pos[2]
         if mouse.device=="stick" then
-            if meow2 then
-                mouse.pos[1] = mouse.pos[1] + (joysticks:getGamepadAxis("rightx")*dt*250)
-                mouse.pos[2] = mouse.pos[2] + (joysticks:getGamepadAxis("righty")*dt*250)
-            else
-                local dx,dy=love.graphics.getDimensions()
-                mouse.pos[1] = (joysticks:getGamepadAxis("rightx")*150)+(dx/2)
-                mouse.pos[2] = (joysticks:getGamepadAxis("righty")*150)+(dy/2)
-            end
-            if mouse.ison(1) or mouse.ison(2) then
-            if not meow then
-                meow=true
-                if mouse.ison(1) then
-                mouse.event(1)
-                else
-                    mouse.event(2)
+            mouse.l = (joysticks:getGamepadAxis("triggerright")~=0)
+            mouse.r = (joysticks:getGamepadAxis("triggerleft")~=0)
+            mouse.pos[1] = mouse.pos[1] + (joysticks:getGamepadAxis("rightx")*dt*250)
+            mouse.pos[2] = mouse.pos[2] + (joysticks:getGamepadAxis("righty")*dt*250)
+            if mouse.ison() or mouse.ison(2) then
+                if not mouse._t then
+                    mouse._t = true
+                    if mouse.ison() then
+                        mouse.event(1)
+                    else
+                        mouse.event(2)
+                    end
                 end
+            else
+                mouse._t = false
             end
         else
-            meow = false
-        end
-        if joysticks:isGamepadDown("rightstick") then
-            if not meow3 then
-                meow2 = not meow2
-                meow3 = true
-            end
-        else
-            meow3=false
-        end
-        end
-        if mouse.device=="mouse" then
+            mouse.l =love.mouse.isDown(1)
+            mouse.r =love.mouse.isDown(2)
             mouse.pos[1],mouse.pos[2]=love.mouse.getPosition()
         end
     end,
     draw = function ()
-        local x = mouse.pos[1]
-        local y = mouse.pos[2]
+        local x, y = mouse.getPos()
         if mouse.device=="stick" then
             love.graphics.setColor(1,1,1)
             love.graphics.polygon("fill",x,y, x,y+25,x+10,y+25, x,y)
@@ -59,48 +43,28 @@ mouse = {
         inv.mouseclick(x,y,b)
         gui.action(x,y,b)
         if b == 1 then
-        if x > 295 and x <= 295+25 and y > 170 and y <= 170+25 then
-            for i = 1,#plr.inventory do
-                if plr.inventory[i].id=="ironore" then
-                    plr.inventory[i].id="crafttable"
-                    sfx.use("woodcut")
-                    break
+            if x > 295 and x <= 295+25 and y > 170 and y <= 170+25 then
+                for i = 1,#plr.inventory do
+                    if plr.inventory[i].id=="ironore" then
+                        plr.inventory[i].id="crafttable"
+                        sfx.use("woodcut")
+                        break
+                    end
                 end
             end
         end
-    end
     end,
     mouseevent = function (b)
         mouse.event(b)
     end,
     ison = function (b)
         local b = b or 1
-        if mouse.device == "stick" then
-            if b == 1 then
-                if joysticks:getGamepadAxis("triggerright")==0 then
-                    return false
-                else
-                    return true
-                end
-            else 
-            if b == 2 then
-                if joysticks:getGamepadAxis("triggerleft")==0 then
-                    return false
-                else
-                    return true
-                end
-            end
+        if b == 1 then
+            return mouse.l
+        elseif b == 2 then
+            return mouse.r
         end
-    else
-        if mouse.device=="mouse" then
-            if love.mouse.isDown(b) then
-                return true
-            else
-                return false
-            end
-        end
-    end
-    return false
+        return false
     end,
     device="mouse",
     pos ={250,250}
