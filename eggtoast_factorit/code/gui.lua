f1e2b303a2dc648883831eb97ac36ac0fc043f2c
Egg Toast <johndoe@example.com>
2024-06-29 19:17:39 +0900

    Removed todo, added improvements

--- eggtoast_factorit/code/gui.lua
+++ eggtoast_factorit/code/gui.lua
@@ -1,8 +1,9 @@
-gui = { --TODO: rewrite entire code here as it sucks
+gui = {
     action = function(x,y,b)
         local tbl = gui.tmpgui or nil
         if nullcheck(tbl) then
             for _, tbl in pairs(tbl) do
+                print(json.encode(tbl))
                 local mx, my = mouse.getPos()
                 local neon = function(a,b)
                     if nullcheck(tbl[a]) then
@@ -18,7 +19,7 @@ gui = { --TODO: rewrite entire code here as it sucks
                     return a
                 end
                 neon("position","p");neon("pos","p");neon("size","s")
-                neon("action","a")
+                neon("action","a");neon("imagesize","s");neon("is","s")
                 local x,y,sx,sy = tbl.p[1],tbl.p[2],tbl.s[1],tbl.s[2]
                 x, y, sx, sy = neo(x), neo(y), neo(sx), neo(sy)
                 x, y, sx, sy = gui.script.func[x[1]](x[2]), gui.script.func[y[1]](y[2]), gui.script.func[sx[1]](sx[2]), gui.script.func[sy[1]](sy[2])
@@ -52,108 +53,121 @@ gui = { --TODO: rewrite entire code here as it sucks
     end,
     tick = function()
     end,
-    draw = function(this)
-        local tbl = this
-        if nullcheck(this) then
-            gui.tmpgui = this
-            for _, this in pairs(this) do
-                local neo = function(a)
-                    if type(a) == "function" then
-                        return {"f",a}
-                    elseif type(a) ~= "table" then
-                        return {"dummy",a}
-                    end
+    draw = function(t)
+        gui.tmpgui = t
+        if nullcheck(t) then
+            local neo = function(a)
+                if type(a) == "function" then
+                    return {"f",a}
+                elseif type(a) ~= "table" then
+                    return {"dummy",a}
+                end
+                return a
+            end
+            local n = function(a,b)
+                if nullcheck(a) then
                     return a
                 end
-                local neon = function(a,b)
-                    if nullcheck(this[a]) then
-                        this[b] = this[a]
-                    end
-                end
-                local ne = function(a,b)
-                    if not nullcheck(this[a]) then
-                        this[a] = b
-                    end
-                end
-                local n = function(a,b)
-                    if nullcheck(a) then
-                        return a
-                    end
-                    return b
-                end
-                local setc = gui.script.setctbl
-                neon("pos","p");neon("size","s");neon("position","p")
-                neon("text","t");neon("render","r")
-                neon("border","b");neon("background","bg")
-                neon("slide","sl");neon("stencil","st")
-                neon("image","i");neon("imagecolor","ic")
-                neon("imagesize","is")
-                neon("imagealpha","ia");neon("borderstencil","bs")
-                if nullcheck(this.b) and #this.b ~= 5 then
-                    if nullcheck(this.b[4]) then
-                        this.b[5] = this.b[4]
-                    else
-                        --this.b[5] = 
-                    end
-                end
-                ne("st",nil);ne("sl",nil)
-                ne("bs",false)
-                local x, y = this.p[1], this.p[2]
-                this.s = this.s or {1,1}
-                local sx, sy = this.s[1], this.s[2]
-                local t = this.t
----@diagnostic disable-next-line: cast-local-type
-                x, y, sx, sy = neo(x), neo(y), neo(sx), neo(sy)
-                x, y, sx, sy = gui.script.func[x[1]](x[2]), gui.script.func[y[1]](y[2]), gui.script.func[sx[1]](sx[2]), gui.script.func[sy[1]](sy[2])
-                if nullcheck(this.bg) then
-                    setc(this.bg)
-                    love.graphics.rectangle("fill",x,y,sx,sy)
-                end
-                if this.bs == true then
-                    love.graphics.stencil(function ()
-                        love.graphics.rectangle("fill",x,y,sx,sy)
-                    end,"replace",1)
-                    love.graphics.setStencilTest("greater", 0)
-                end
-                if nullcheck(this.r) then
-                    this.r()
-                end
-                if nullcheck(this.i) then
-                    this.ic = this.ic or {1,1,1,1}
-                    this.ia = this.ia or this.ic[4] or 1
-                    this.is = this.is or {1,1}
-                    gui.script.setctbl(this.ic)
-                    love.graphics.draw(texture.gettex(this.i),x,y,0,this.is[1],this.is[2])
-                end
-                if nullcheck(t) then
-                    t.t = n(t.text,t.t)
-                    t.c = n(t.color,t.c)
-                    t.s = n(t.size,t.s)
-                    gui.script.setctbl(t.c)
-                    if string.sub(t.t,1,1) == "#" then
-                        t.t = lang.gettxt(string.sub(t.t,2,string.len(t.t)))
-                    end
-                    love.graphics.print(t.t,x+t.p[1],y+t.p[2],0,t.s[1],t.s[2])
-                end
-                if nullcheck(this.b) then
-                    love.graphics.setColor(this.b[2],this.b[3],this.b[4],n(this.b[5],1))
-                    if this.b[1] == 1 then
-                        love.graphics.rectangle("line",x,y,sx,sy)
-                    else
-                        local i = this.b[1]
-                        love.graphics.rectangle("fill",x,y,i,sy)--left
-                        love.graphics.rectangle("fill",x+i,y,sx-i,i)--top
-                        love.graphics.rectangle("fill",x+i,y+sy-i,sx-i,i)--bottom
-                        love.graphics.rectangle("fill",x+sx-i,y+i,i,sy-i-i)--right
-                    end
-                end
-                if this.bs == true then
-                    love.graphics.setStencilTest()
-                end
+                return b
             end
-            love.graphics.setColor(1,1,1)
+            local setc = gui.script.setctbl
+            local g = {}
+            if nullcheck(t[g]) then
+                g = t[g]
+            end
+            for k, this in pairs(t) do
+                --This single line makes literaly no sense in first glance. Because it does not make any sense.
+                if not nullcheck(k) then elseif type(k) == "string" then elseif not ((k == "g") or (k == "global")) then
+                    --Note: I know implimenting functions like this is bad.
+                    --But Image:getData() don't want VScode and lua complaining about it.
+                    --Screw you autocorrect ( why not leave it as is? )(why did i accidentally added space)
+                    local neon = function(a,b)
+                        if nullcheck(this[a]) then
+                            this[b] = this[a]
+                        end
+                    end
+                    local ne = function(a,b)
+                        if not nullcheck(this[a]) then
+                            this[a] = b
+                        end
+                    end
+                    neon("pos","p");neon("size","s");neon("position","p")
+                    neon("text","t");neon("render","r")
+                    neon("border","b");neon("background","bg")
+                    neon("slide","sl");neon("stencil","st")
+                    neon("image","i");neon("imagecolor","ic")
+                    neon("imagesize","is")
+                    neon("imagealpha","ia");neon("borderstencil","bs")
+                    if nullcheck(this.b) and #this.b ~= 5 then
+                        if nullcheck(this.b[4]) then
+                            this.b[5] = this.b[4]
+                        else
+                            --this.b[5] = 
+                        end
+                    end
+                    if not nullcheck(this.is) then
+                        this.is = this.s
+                    end
+                    ne("st",nil);ne("sl",nil)
+                    ne("bs",false)
+                    local x, y = this.p[1], this.p[2]
+                    this.s = this.s or {1,1}
+                    local sx, sy = this.s[1], this.s[2]
+                    local t = this.t
+    ---@diagnostic disable-next-line: cast-local-type
+                    x, y, sx, sy = neo(x), neo(y), neo(sx), neo(sy)
+                    x, y, sx, sy = gui.script.func[x[1]](x[2]), gui.script.func[y[1]](y[2]), gui.script.func[sx[1]](sx[2]), gui.script.func[sy[1]](sy[2])
+                    if nullcheck(this.bg) then
+                        setc(this.bg)
+                        love.graphics.rectangle("fill",x,y,sx,sy)
+                    end
+                    if this.bs == true then
+                        love.graphics.stencil(function ()
+                            love.graphics.rectangle("fill",x,y,sx,sy)
+                        end,"replace",1)
+                        love.graphics.setStencilTest("greater", 0)
+                    end
+                    if nullcheck(this.r) then
+                        this.r()
+                    end
+                    if nullcheck(this.i) then
+                        this.ic = this.ic or {1,1,1,1}
+                        this.ia = this.ia or this.ic[4] or 1
+                        this.is = this.is or {1,1}
+                        gui.script.setctbl(this.ic)
+                        love.graphics.draw(texture.gettex(this.i),x,y,0,this.is[1],this.is[2])
+                    end
+                    if nullcheck(t) then
+                        t.t = n(t.text,t.t)
+                        t.c = n(t.color,t.c)
+                        t.s = n(t.size,t.s)
+                        gui.script.setctbl(t.c)
+                        if string.sub(t.t,1,1) == "#" then
+                            t.t = lang.gettxt(string.sub(t.t,2,string.len(t.t)))
+                        end
+                        love.graphics.print(t.t,x+t.p[1],y+t.p[2],0,t.s[1],t.s[2])
+                    end
+                    if nullcheck(this.b) then
+                        love.graphics.setColor(this.b[2],this.b[3],this.b[4],n(this.b[5],1))
+                        if this.b[1] == 1 then
+                            love.graphics.rectangle("line",x,y,sx,sy)
+                        else
+                            local i = this.b[1]
+                            love.graphics.rectangle("fill",x,y,i,sy)--left
+                            love.graphics.rectangle("fill",x+i,y,sx-i,i)--top
+                            love.graphics.rectangle("fill",x+i,y+sy-i,sx-i,i)--bottom
+                            love.graphics.rectangle("fill",x+sx-i,y+i,i,sy-i-i)--right
+                        end
+                    end
+                    if this.bs == true then
+                        love.graphics.setStencilTest()
+                    end
+                end
+                love.graphics.setColor(1,1,1)
+            end
         end
-        if table.haskey(this,"slider") then
+        local tbl = t
+        if table.haskey(t,"slider") then
             for i = 1,#tbl.slider do
                 local this = tbl.slider[i]
                 local quartcircle = math.pi
