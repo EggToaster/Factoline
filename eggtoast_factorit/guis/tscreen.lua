3749f94b76afcc669123ffde098e31a822effb37
Egg Toast <johndoe@example.com>
2024-06-29 19:20:56 +0900

    Minor adjustment to show minor adjustment

--- eggtoast_factorit/guis/tscreen.lua
+++ eggtoast_factorit/guis/tscreen.lua
@@ -7,7 +7,7 @@ local this ={
     bg={1,1,1},b={3,0,0,0},
     t={t="#title.settingsbutton",c={0,0,0},s={1.5,1.5},
     p={0,0}},action=function()stng=true;stngtab="general";end},
-    {p={function()return getdx()/2-350;end,5},is={0.7,0.7},
+    {p={function()return getdx()/2-350;end,5},s={0.7,0.7},
     i="factorylogo"}
 }
 return this
